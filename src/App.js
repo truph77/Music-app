32a872dfcc7f30da2c3e0324671a2c4d4d7f286c
Trung Pham <phamtrung2422k@gmail.com>
2020-12-15 18:12:11 +0700

    design Player components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import { useState } from "react";
-
-import { useState } from "react";
+import Player from "./Components/Player";
 
 function App() {
   const [songs, setSongs] = useState([
     {
       title: "Somethings just like this",
-      artist: "The chainsmokes",
+      artist: "The-Chainsmokers-ft-Coldplay",
       img_src: "./images/song-1.png",
-      src: "./music/somethings-just-like-this.mp3",
+      src: "./music/something-just-like-this.mp3",
+    },
+    {
+      title: "Memory",
+      artist: "The-Chainsmokers-ft-Daya",
+      img_src: "./images/song-2.png",
+      src: "./music/Dont-let-me-down.mp3",
     },
   ]);
-  return <div className="App">Components here!!!</div>;
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
+
+  return (
+    <div className="App">
+      <Player song={songs[currentSongIndex]} nextSong={songs[nextSongIndex]} />
+    </div>
+  );
 }
 
 export default App;
